92dfa0da197a91bd71db6692aacb0e33873b5f91
Mingkai Pang <pangmin@vt.edu>
2022-03-24 17:01:38 -0400

    implemented thread_pool_new and destroy

--- tests/threadpool.c
+++ tests/threadpool.c
@@ -44,8 +44,37 @@ struct worker {
 
 static __thread struct worker *current_worker;
 
+
 /* Create a new thread pool with no more than n threads. */
-struct thread_pool *thread_pool_new(int nthreads);
+struct thread_pool *thread_pool_new(int nthreads) {
+    struct thread_pool *pool = malloc(sizeof(struct thread_pool));
+
+    list_init(&pool->global_queue);
+    list_init(&pool->worker_list);
+
+    pthread_mutex_init(&pool->pool_mutex, NULL);
+    pthread_mutex_init(&pool->worker_mutex, NULL);
+    pthread_cond_init(&pool->pool_cond, NULL);
+
+    pool->destroy = 0;
+    pool->njobs = 0;
+    pool->nthreads = nthreads;
+
+    pthread_mutex_lock(&pool->pool_mutex);
+    struct worker *worker;
+    for (int i = 0; i < nthreads; i++) {
+        worker = malloc(sizeof(struct worker));
+        pthread_mutex_init(&worker->local_mutex, NULL);
+        list_init(&worker->worker_queue);
+        list_push_front(&pool->worker_list, &worker->elem);
+        pthread_create(&worker->threadID, NULL, start_routine, pool);
+    }
+    current_worker = NULL;
+
+    pthread_mutex_unlock(&pool->pool_mutex);
+    // free(worker);
+    return pool;
+}
 
 /*
  * Shutdown this thread pool in an orderly fashion.
@@ -54,7 +83,27 @@ struct thread_pool *thread_pool_new(int nthreads);
  *
  * Deallocate the thread pool object before returning.
  */
-void thread_pool_shutdown_and_destroy(struct thread_pool *);
+void thread_pool_shutdown_and_destroy(struct thread_pool *pool) {
+    pthread_mutex_lock(&pool->pool_mutex);
+    pool->destroy = 1;
+    pthread_cond_broadcast(&pool->pool_cond);
+    pthread_mutex_unlock(&pool->pool_mutex);
+
+    for (struct list_elem *element = list_begin(&pool->worker_list);
+         element != list_end(&pool->worker_list);
+         element = list_next(element)) {
+        pthread_join(list_entry(element, struct worker, elem)->threadID, NULL);
+    }
+    for (struct list_elem *element = list_begin(&pool->worker_list);
+         element != list_end(&pool->worker_list);) {
+        struct worker *worker = list_entry(element, struct worker, elem);
+        element = list_remove(element);
+        free(worker);
+        // element = list_next(element);
+    }
+
+    free(pool);
+}
 
 /*
  * Submit a fork join task to the thread pool and return a
@@ -67,14 +116,18 @@ void thread_pool_shutdown_and_destroy(struct thread_pool *);
  * Returns a future representing this computation.
  */
 struct future *thread_pool_submit(struct thread_pool *pool,
-                                  fork_join_task_t task, void *data);
+                                  fork_join_task_t task, void *data) {
+}
 
 /* Make sure that the thread pool has completed the execution
  * of the fork join task this future represents.
  *
+ * 2 = UNSCHEDULE, 1 = EXECUTING, 0 = FINISHED
+ *
  * Returns the value returned by this task.
  */
-void *future_get(struct future *);
+void *future_get(struct future *future) {
+}
 
 /* Deallocate this future.  Must be called after future_get() */
-void future_free(struct future *);
+void future_free(struct future *future) { free(future); }
